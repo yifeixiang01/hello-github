2a03eb1b6f1f70506aa27e3780497b588fc67d14
yifeixiang01 <yifeixiang0101@163.com>
2020-03-28 13:19:36 +0800

    init project

---
+++ src/config/radar-config.js
@@ -0,0 +1,94 @@
+export var NormalRadarInfo = [
+    {
+        "label": "完整",
+        "value": 65
+    }, {
+        "label": "幸福",
+        "value": 90
+    }, {
+        "label": "廉价感",
+        "value": 20
+    }, {
+        "label": "SAN",
+        "value": 95
+    }, {
+        "label": "悲伤",
+        "value": 30
+    }, {
+        "label": "自敬感",
+        "value": 70
+    }
+];
+export var JobRadarInfo = [
+    {
+        "label": "Dominance",
+        "max": 25,
+        "value": 19
+    }, {
+        "label": "I",
+        "max": 25,
+        "value": 3
+    }, {
+        "label": "Steadiness",
+        "max": 25,
+        "value": 3
+    }, {
+        "label": "C",
+        "max": 25,
+        "value": 15
+    }
+];
+export var SkillRadarInfo = [
+    {
+        "label": "CSS",
+        "max": 120,
+        "value": 88
+    }, {
+        "label": "Java-\nScript",
+        "max": 120,
+        "value": 113
+    }, {
+        "label": "Node.js",
+        "max": 120,
+        "value": 70
+    }, {
+        "label": "PHP",
+        "max": 120,
+        "value": 65
+    }, {
+        "label": "DB",
+        "max": 120,
+        "value": 60
+    }, {
+        "label": "UI设计",
+        "max": 120,
+        "value": 82
+    }
+];
+export var AcgRadarInfo = [
+    {
+        "label": "生命",
+        "value": 80,
+        "max": 100,
+    }, {
+        "label": "爱",
+        "value": 59,
+        "max": 100,
+    }, {
+        "label": "恐惧",
+        "value": 80,
+        "max": 100,
+    }, {
+        "label": "理性",
+        "value": 70,
+        "max": 100,
+    }, {
+        "label": "疼痛",
+        "value": 30,
+        "max": 100,
+    }, {
+        "label": "期望",
+        "value": 10,
+        "max": 100,
+    }
+];
